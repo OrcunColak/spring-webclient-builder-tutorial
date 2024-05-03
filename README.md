54710591ca9a21b716b16b19450fec2b80026045
Orçun Çolak <orcun.colak@hazelcast.com>
2024-05-03 14:34:02 +0300

    upgrade spring

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Read Me First
+
+WebClient.builder that uses resilience4j library
+
 The original idea is from  
 https://medium.com/@yunussov/enhanced-spring-webflux-webclient-b5deeebdf8ee
--- src/main/java/com/colak/springwebclientbuildertutorial/SpringWebClientBuilderTutorialApplication.java
+++ src/main/java/com/colak/springwebclientbuildertutorial/SpringWebClientBuilderTutorialApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringWebclientBuilderTutorialApplication {
+public class SpringWebClientBuilderTutorialApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringWebclientBuilderTutorialApplication.class, args);
+		SpringApplication.run(SpringWebClientBuilderTutorialApplication.class, args);
 	}
 
 }
